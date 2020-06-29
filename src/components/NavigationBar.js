c4db0030d8a4d9ecee9079738e3b86c3fb020b62
thedualspace <64597895+thedualspace@users.noreply.github.com>
2020-06-29 19:22:56 +0100

    navbar color edit

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,7 +22,7 @@ const Styles = styled.div`
 
  export const NavigationBar = () => (
    <Styles>
-     <Navbar expand='lg'>
+     <Navbar expand='lg' variant="dark" >
        <Navbar.Brand href="/dashboard">Code Life</Navbar.Brand>
        <Navbar.Toggle aria-controls='basic-navbar-nav' />
        <Navbar.Collapse id='basic-navbar-nav'>
